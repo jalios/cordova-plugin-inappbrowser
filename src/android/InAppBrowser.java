1d4a49470fce1f59d837f2df6c38b8db352de545
Slayterik <59745613+Slayterik@users.noreply.github.com>
2020-03-20 17:07:48 +0300

    Android GH-470 InAppBrowser: java.lang.IllegalArgumentException (#616)
    Fix interacting with views when Activity destroyed
    Bug description https://stackoverflow.com/questions/22924825/view-not-attached-to-window-manager-crash
    
    Co-authored-by: Sarafanov Valeriy <sarafanov_v@firma-gamma.ru>

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -316,7 +316,7 @@ public class InAppBrowser extends CordovaPlugin {
             this.cordova.getActivity().runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    if (dialog != null) {
+                    if (dialog != null && !cordova.getActivity().isFinishing()) {
                         dialog.show();
                     }
                 }
@@ -329,7 +329,7 @@ public class InAppBrowser extends CordovaPlugin {
             this.cordova.getActivity().runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    if (dialog != null) {
+                    if (dialog != null && !cordova.getActivity().isFinishing()) {
                         dialog.hide();
                     }
                 }
@@ -565,7 +565,7 @@ public class InAppBrowser extends CordovaPlugin {
                 childView.setWebViewClient(new WebViewClient() {
                     // NB: wait for about:blank before dismissing
                     public void onPageFinished(WebView view, String url) {
-                        if (dialog != null) {
+                        if (dialog != null && !cordova.getActivity().isFinishing()) {
                             dialog.dismiss();
                             dialog = null;
                         }
